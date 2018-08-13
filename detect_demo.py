1b222fd36a489ab1f98435bfe5f05993468c87e6
yiping <yiping.chen@hypereal.com>
2018-08-13 11:45:44 +0800

    add hough

--- detect_demo.py
+++ detect_demo.py
@@ -6,9 +6,9 @@ import random
 import sys
 import image_utils
 
-def init_random_square(size, length):
+def init_random_square(size, length, ratio=1):
     img = np.zeros((size, size))
-    img[int(size/2-length/2):int(size/2+length/2), int(size/2-length/2):int(size/2+length/2)] = 255
+    img[int(size/2-length/2):int(size/2+length/2), int(size/2-length/2*ratio):int(size/2+length/2*ratio)] = 255
 
     degree = random.randint(0, 360)
     print('degree: {}'.format(degree))
@@ -20,13 +20,52 @@ def init_random_square(size, length):
 
     return img, degree
 
-def test_random():
-    square, ori_angle = init_random_square(256, 60)
+def hough_test_random():
+    square, ori_angle = init_random_square(256, 60, 0.7)
+    square = image_utils.add_random_noise(square, 30)
+    square = np.uint8(square)
+    
+    edges = cv.Canny(square, 50, 150)
+    minLineLength = 20
+    maxLineGap = 10
+    lines = cv.HoughLinesP(edges, 1, np.pi/180, 20, minLineLength, maxLineGap)
+
+    for i in range(len(lines)):
+        for x1, y1, x2, y2 in lines[i]:
+            cv.line(square, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            angle = math.atan2((y2 - y1), (x2 - x1)) / math.pi * 180
+            print('Predict: {}'.format(angle))
+
+    cv.imshow('hough test', square)
+    cv.waitKey(0)
+
+def hough_test_real():
+    square = cv.imread(sys.argv[1], 0)
+    square = np.uint8(square)
+    
+    edges = cv.Canny(square, 50, 150)
+    minLineLength = 50
+    maxLineGap = 10
+    lines = cv.HoughLinesP(edges, 1, np.pi/180, 20, minLineLength, maxLineGap)
+
+    for i in range(len(lines)):
+        for x1, y1, x2, y2 in lines[i]:
+            cv.line(square, (x1, y1), (x2, y2), (0, 128, 0), 2)
+            angle = math.atan2((y2 - y1), (x2 - x1)) / math.pi * 180
+            print('Predict: {}'.format(angle))
+
+    cv.resize(square, (512, 512), interpolation = cv.INTER_CUBIC)
+    cv.imshow('hough test', square)
+    cv.waitKey(0)
+
+def fft_test_random():
+    square, ori_angle = init_random_square(256, 60, 0.7)
     square = image_utils.add_random_noise(square, 30)
 
     fimg = np.fft.fft2(square)
     fshift = np.fft.fftshift(fimg)
     mag_spec = np.log(np.abs(fshift))
+    ph_spec = np.angle(fshift)
 
     mag_spec[70:186, 70:186] = 0
     top_num =5
@@ -55,11 +94,12 @@ def test_random():
     total_error = total_error / top_num
     print('Prediction Error: {}'.format(total_error))
 
-    plt.subplot(121), plt.imshow(square, cmap='gray')
-    plt.subplot(122), plt.imshow(mag_spec, cmap='gray')
+    plt.subplot(131), plt.imshow(square, cmap='gray')
+    plt.subplot(132), plt.imshow(mag_spec, cmap='gray')
+    plt.subplot(133), plt.imshow(ph_spec, cmap='gray')
     plt.show()
 
-def test_real():
+def fft_test_real():
     square = cv.imread(sys.argv[1], 0)
     print(np.mean(square))
     square = square - np.mean(square)
@@ -74,6 +114,7 @@ def test_real():
     fimg = np.fft.fft2(square)
     fshift = np.fft.fftshift(fimg)
     mag_spec = np.log(np.abs(fshift))
+    ph_spec = np.angle(fshift)
 
     mag_spec[int(img_height/2*0.9):int(img_height/2*1.1), int(img_width/2*0.9):int(img_width/2*1.1)] = 0
     top_num =5
@@ -99,11 +140,14 @@ def test_real():
         angle = -math.atan2((max_y_stack[i] - img_height/2), (max_x_stack[i] - img_width/2)) / math.pi * 180
         print('Angle: {}'.format(angle))
 
-    plt.subplot(121), plt.imshow(square, cmap='gray')
-    plt.subplot(122), plt.imshow(mag_spec, cmap='gray')
+    plt.subplot(131), plt.imshow(square, cmap='gray')
+    plt.subplot(132), plt.imshow(mag_spec, cmap='gray')
+    plt.subplot(133), plt.imshow(ph_spec, cmap='gray')
     plt.show()
 
 if __name__ == '__main__':
 #    for i in range(10):
-#        test_random()
-     test_real()
+#        fft_test_random()
+#        hough_test_random()
+#     fft_test_real()
+     hough_test_real()
