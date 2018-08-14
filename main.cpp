d3a4306cab8ba8af91ef214f883af50ce0d6ac8b
Chen <yiping.chen@hypereal.local>
2018-08-14 19:42:19 +0800

    fix bug

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 //
 // Created by yiping.chen on 2018/8/14.
 //
-
-#include "hough.hpp"
 #include <iostream>
+#include "hough.hpp"
 
 using namespace std;
 
@@ -13,10 +12,10 @@ int main() {
     cout << img.step << endl;
 
     HoughLines Houghdetector(img, 20, 1, 1/180*CV_PI);
-    Houghlines::PolarLine lines = HoughLines.getLines(5);
+    vector<HoughLines::PolarLine> lines = HoughLines.getLines(5);
 
     for (int i = 0; i < 5; i++) {
-        cout << 'deteted lines: ' << endl;
+        cout << "deteted lines: " << endl;
         cout << lines[i].rho << ' ' << lines[0].theta << ' ' << lines[i].cout << endl;
     }
 }
